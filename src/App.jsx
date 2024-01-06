f6020f303bfd60c6966fce01bace45b3d1643c1f
Joe Rutkowski <jbrutkowski77@gmail.com>
2024-01-05 20:46:21 -0600

    Added Typing Element, added FB/Twitter/IG/Linkedin links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Sidenav from './components/sidenav'
-import Main from './components/main'
+import Sidenav from './components/sidenav.jsx'
+import Main from './components/main.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,34 @@
 import React from "react"
-
+import { TypeAnimation } from "react-type-animation"
+import {FaTwitter, FaFacebookF, FaLinkedin, FaInstagram} from 'react-icons-fa'
 const Main =() => {
     return (
         <div id='main'>
             <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-waves-z7N8YMSM-v4"></img>
             <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
                 <h1>Joe's Here!</h1>
-                <h2>I'm a software engineering professional looking to help you!</h2>
+                <h2>I'm a 
+                    <TypeAnimation
+                    sequence={[
+                        'Web Developer',
+                        2000,
+                        'Scrum Master',
+                        2000,
+                        'Consultant',
+                        2000,
+                    ]}
+                    wrapper='div'
+                    cursor={true}
+                    repeat={infinity}
+                    style={{fontSize: '1em', paddingLeft: '5px'}}
+                    />
+                </h2>
+                <div>
+                    <FaTwitter className='cursor-pointer' size={20}/>
+                    <FaFacebookF className='cursor-pointer' size={20}/>
+                    <FaInstagram className='cursor-pointer' size={20}/>
+                    <FaLinkedin className='cursor-pointer' size={20}/>
+                </div>
             </div>
         </div>
     )
