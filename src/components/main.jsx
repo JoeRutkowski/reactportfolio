ea7c2ca4289e6fd5d46ea793c5dd50cb31657132
Joe Rutkowski <jbrutkowski77@gmail.com>
2024-01-06 21:38:12 -0600

    Adding formatting to main page text

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,10 +4,12 @@ import {FaTwitter, FaFacebookF, FaLinkedin, FaInstagram} from 'react-icons-fa'
 const Main =() => {
     return (
         <div id='main'>
-            <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-waves-z7N8YMSM-v4"></img>
+            <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-waves-z7N8YMSM-v4"
+            />
             <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
-                <h1>Joe's Here!</h1>
-                <h2>I'm a 
+                <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center"></div>
+                <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Joe's Here!</h1>
+                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a 
                     <TypeAnimation
                     sequence={[
                         'Web Developer',
@@ -23,7 +25,7 @@ const Main =() => {
                     style={{fontSize: '1em', paddingLeft: '5px'}}
                     />
                 </h2>
-                <div>
+                <div className='flex justify-between pt-6 max-w-[200px] w-full'>
                     <FaTwitter className='cursor-pointer' size={20}/>
                     <FaFacebookF className='cursor-pointer' size={20}/>
                     <FaInstagram className='cursor-pointer' size={20}/>
