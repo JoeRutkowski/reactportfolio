2c76e7a79132a37836107c3b3f1aa0b9d9462728
Joe Rutkowski <jbrutkowski77@gmail.com>
2024-01-07 23:01:08 -0600

    Create Work File

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Sidenav from './components/sidenav.jsx'
 import Main from './components/main.jsx'
 
