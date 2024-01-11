7e381ce290ec23b7cea86fcb61b5b9568c7f1565
Joe Rutkowski <jbrutkowski77@gmail.com>
2024-01-10 20:44:34 -0600

    data mapping?

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -17,7 +17,12 @@ const data = [
 const Work = () => {
     return (
         <div id='work' classname='max-w-[1040px] n-auto md:pl-20 p4 py-16'>
-            <h1> work </h1>
+            <h1 classname="text-4xl font-bold text-center text-[#001b5e]"> Work </h1>
+            {
+                data.map(item, idx) => (
+                    <WorkItem key={idx} year={item.Year} title={item.Title} Duration={item.Duration} Details={item.Details}/>
+    )
+            }
         </div>
     )
 }
