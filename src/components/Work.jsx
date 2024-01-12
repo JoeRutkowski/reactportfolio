cf612f5a311792a746c0549a6bd11783198b740d
Joe Rutkowski <jbrutkowski77@gmail.com>
2024-01-11 21:53:52 -0600

    Work and Workitem

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,15 +13,15 @@ const data = [
             Duration: 'filler text',
             Details: 'filler text'
          }
-]
+];
 const Work = () => {
     return (
-        <div id='work' classname='max-w-[1040px] n-auto md:pl-20 p4 py-16'>
+        <div id='work' classname='max-w-[1040px] m-auto md:pl-20 p4 py-16'>
             <h1 classname="text-4xl font-bold text-center text-[#001b5e]"> Work </h1>
             {
-                data.map(item, idx) => (
+                data.map((item, idx) => (
                     <WorkItem key={idx} year={item.Year} title={item.Title} Duration={item.Duration} Details={item.Details}/>
-    )
+                ))
             }
         </div>
     )
