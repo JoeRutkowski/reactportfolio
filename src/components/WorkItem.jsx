dda7a50c87f6dd057f844f9f839b3292133899fd
Joe Rutkowski <jbrutkowski77@gmail.com>
2024-01-12 23:00:21 -0600

    Styling on work section

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -5,11 +5,11 @@ const WorkItem = (year, title, duration, details) => {
             <li className='mb-10 ml-4'>
                 <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white'/>
                 <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
-                    <span>{year}</span>
-                    <span>{title}</span>
-                    <span>{duration}</span>
+                    <span className='inlin-block px-2 py-1 font-semibold text-white bg-[#001b53] rounded-md'>{year}</span>
+                    <span className='text-lg font-semibold text-[#001b5e]'>{title}</span>
+                    <span className='ny-1 text-sm font-normal leading-none text-stone-400'>{duration}</span>
                 </p>
-                <p> {details}</p>
+                <p className='ny-2 text-base font-normal text-stone-500'> {details}</p>
             </li>
         </ol>
     )
